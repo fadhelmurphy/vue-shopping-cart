672dfccfeabb301ba4630e26b01ceb6264f6b9dc
unknown <krazefollow@gmail.com>
2021-11-03 13:24:00 +0700

    EZ PZ

--- src/components/layouts/Header.vue
+++ src/components/layouts/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container flex flex-wrap mx-auto my-40 px-5 min-h-96 items-center">
         <h1 class="header-font mb-20 text-6xl text-center font-bold md:text-8xl md:text-left">Selamat datang di Mamang shop</h1>
-      <p class="text-gray-400">Web ini dibuat menggunakan Vue,Vuex,Tailwind</p>
+      <p class="text-center text-gray-400 md:text-left">Web ini dibuat menggunakan Vue,Vuex,Tailwind</p>
     </div>
 </template>
 <style scoped>
